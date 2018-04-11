8a46e591bc4fbbd1616188bc359ebc8173e88dae
Papa Beye <32583389+PapaBeye@users.noreply.github.com>
2018-04-11 06:01:28 -0400

    stellarium remote control test

--- webroot/js/api/trunc.js
+++
@@ -1,8 +0,0 @@
-define(function() {
-	"use strict";
-	
-	//ensure that the browser has a trunc function
-	Math.trunc = Math.trunc || function(x) {
-		return x < 0 ? Math.ceil(x) : Math.floor(x);
-	};
-});
